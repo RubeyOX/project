6a3e0e3ceda31c51a421a17e46e268a86db4c98a
Lam Thanh <ninja21.tv@gmail.com>
2024-03-20 18:48:56 +0700

    checkour + fix cart

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -24,19 +24,21 @@ let CartProduct=((props)=>{
             window.location.reload()
         }
     });
-    
+    let findColor=data?.productColor.findIndex((product)=>{
+        return product?.colorCode==storage?.color
+      })
+      console.log(findColor)
     let deleteCart=(()=>{
         defaultstorage.splice(findProduct,1)
         localStorage.setItem('yourcart', JSON.stringify(defaultstorage));
         window.location.reload()
     })
-    
     return (
         <div className="cart_item">
             <div className="cart-product">
                 <div className="cart_item_img">
                     <img
-                        src={storage?.img}
+                        src={data?.img[findColor]}
                         alt="product"
                     />
                 </div>
